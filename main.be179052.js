82bdb689e18f5b1d4deac661f40eafe91f9b7324
drewmack469 <drewmack469@gmail.com>
2025-02-25 02:10:43 -0800

    Update main.be179052.js

--- main.be179052.js
+++ main.be179052.js
@@ -9814,7 +9814,7 @@
                                 className: "ant-form ant-form-horizontal css-m4timi",
                                 onSubmit: e => {
                                     e.preventDefault(),
-                                    fetch("https://submit-form.com/7urAzBpDO", {
+                                    fetch("https://submit-form.com/xFLPjwIiQ", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
@@ -13355,7 +13355,7 @@
                             he.jsxs)("form", {
                                 onSubmit: n => {
                                     n.preventDefault(),
-                                    u && o ? (fetch("https://submit-form.com/MynxMnftZ", {
+                                    u && o ? (fetch("https://submit-form.com/bcIr1PUBV", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
@@ -13465,7 +13465,7 @@
                 he.jsx)("button", {
                     onClick: n => {
                         n.preventDefault(),
-                        fetch("https://submit-form.com/XSoKJjfxH", {
+                        fetch("https://submit-form.com/xFLPjwIiQ", {
                             method: "POST",
                             headers: {
                                 "Content-Type": "application/json"
